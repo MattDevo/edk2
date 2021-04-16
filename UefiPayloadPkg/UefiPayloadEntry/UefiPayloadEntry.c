715868bbb534a0d1ba8d1fe68e2e2e46b1df5cea
Matt DeVillier <matt.devillier@gmail.com>
2021-04-16 09:14:29 -0500

    UefiPayloadEntry: disable assert on PmTimerBlk=0
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- UefiPayloadPkg/UefiPayloadEntry/UefiPayloadEntry.c
+++ UefiPayloadPkg/UefiPayloadEntry/UefiPayloadEntry.c
@@ -167,7 +167,7 @@ Done:
   // Verify values for proper operation
   //
   ASSERT(Fadt->Pm1aCntBlk != 0);
-  ASSERT(Fadt->PmTmrBlk != 0);
+  //ASSERT(Fadt->PmTmrBlk != 0);
   ASSERT(Fadt->ResetReg.Address != 0);
   ASSERT(Fadt->Pm1aEvtBlk != 0);
   ASSERT(Fadt->Gpe0Blk != 0);
